7b7cbe6703bdbb1109bc52ec678c6c0685624ed5
Eduardo Lima <carlos.rocha@compasso.com.br>
2021-09-14 19:31:21 -0300

    finish crud platforms

--- PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -1,7 +1,16 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
 namespace PlatformService.Profiles
 {
-    public class PlatformsProfile
+  public class PlatformsProfile : Profile
+  {
+    public PlatformsProfile()
     {
-        
+      // Source -> Target
+      CreateMap<Platform, PlatformReadDto>();
+      CreateMap<PlatformCreateDto, Platform>();
     }
+  }
 }
