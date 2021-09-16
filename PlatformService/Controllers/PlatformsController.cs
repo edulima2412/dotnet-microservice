6fc84d8038f5455e39985aeacf4a5b4f3afe53a3
Eduardo Lima <carlos.rocha@compasso.com.br>
2021-09-15 21:25:04 -0300

    config calls http

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
 using PlatformService.Models;
+using PlatformService.SyncDataServices.Http;
 
 namespace PlatformService.Controllers
 {
@@ -14,11 +16,13 @@ namespace PlatformService.Controllers
   {
     private readonly IPlatformRepository _repository;
     private readonly IMapper _mapper;
+    private readonly ICommandDataClient _commandDataClient;
 
-    public PlatformsController(IPlatformRepository repository, IMapper mapper)
+    public PlatformsController(IPlatformRepository repository, IMapper mapper, ICommandDataClient commandDataClient)
     {
       _repository = repository;
       _mapper = mapper;
+      _commandDataClient = commandDataClient;
     }
 
     [HttpGet]
@@ -45,7 +49,7 @@ namespace PlatformService.Controllers
     }
 
     [HttpPost]
-    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+    public async Task<ActionResult<PlatformReadDto>> CreatePlatform(PlatformCreateDto platformCreateDto)
     {
       var platformModel = _mapper.Map<Platform>(platformCreateDto);
 
@@ -53,6 +57,16 @@ namespace PlatformService.Controllers
       _repository.SaveChanges();
 
       var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+      try
+      {
+        await _commandDataClient.SendPlatformCommand(platformReadDto);
+
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"--> Could not send synchronously! {ex.Message}");
+      }
 
       return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
     }
